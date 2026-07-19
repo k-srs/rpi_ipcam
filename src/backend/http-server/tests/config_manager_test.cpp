295238cbe540033e0de8e6f672ac5f2f37c332bd
k-srs <srs.23p2.1123@gmail.com>
2026-07-19 14:04:03 +0900

    test: width、height を取得するテストを作成

--- src/backend/http-server/tests/config_manager_test.cpp
+++ src/backend/http-server/tests/config_manager_test.cpp
@@ -1,16 +1,29 @@
 #include <CppUTest/TestHarness.h>
+#include <stdexcept>
 
 #include "config/config_manager.hpp"
 
 TEST_GROUP(ConfigManagerTest)
 {};
 
+// キーに対応した値を取得できる
 TEST(ConfigManagerTest, ReturnsValueForKey)
 {
     ConfigManager manager;
-    int64_t expected = 3;
+    int64_t expected_width = 3;
+    int64_t expected_height = 4;
 
     const auto width = manager.get("capture.width");
+    const auto height = manager.get("capture.height");
 
-    LONGS_EQUAL(expected, width);
+    LONGS_EQUAL(expected_width, width);
+    LONGS_EQUAL(expected_height, height);
+}
+
+// 存在しないキーの場合は例外が発生する
+TEST(ConfigManagerTest, ThrowsWhenKeyDoesNotExist)
+{
+    ConfigManager manager;
+
+    CHECK_THROWS(std::out_of_range, manager.get("unknown.key"));
 }
